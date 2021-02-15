229d6f10b12ec8ab73d3dfbca2586f5026671192
johnthomsonn <johnthomson16@hotmail.co.uk>
2021-02-15 22:11:56 +0000

    timer stuff

--- CoinCollector.py
+++ CoinCollector.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, time
 from Player import Player
 from Coin import Coin
 pygame.font.init()
@@ -16,10 +16,19 @@ MONEY_FONT = pygame.font.SysFont("comicsans", 40)
 MAX_COINS = 7
 coins = []
 
+ROUND_TIME = 7
+
 #user events for coin hits
 SMALL_COIN_HIT = pygame.USEREVENT + 1
 MEDIUM_COIN_HIT = pygame.USEREVENT + 2
 LARGE_COIN_HIT = pygame.USEREVENT + 3
+
+#game variables
+startTime = time.time()
+endTime = startTime + ROUND_TIME
+inRound = False
+timeLeft = 0
+player = None
 
 def handle_movement(keys_pressed,player):
     player.move(keys_pressed,WIN)
@@ -39,10 +48,13 @@ def try_generate_coins():
 
 
 
-def draw_window(player, coins):
+def draw_window(player, coins, timeLeft):
     WIN.fill(WINDOW_COLOUR)    
     money_text = MONEY_FONT.render("Money: £" + str(player.money), 1, FONT_COLOUR)
+    
+    timer_text = MONEY_FONT.render("" + str(timeLeft // 1), 1, FONT_COLOUR)
     WIN.blit(money_text, (10,10))
+    WIN.blit(timer_text, (WIDTH-200,10))
     player.draw(WIN)
     for coin in coins:
         coin.draw(WIN)
@@ -50,11 +62,34 @@ def draw_window(player, coins):
 
     pygame.display.update()
 
+
+
+def start_round():
+    global inRound, startTime, endTime, timeLeft
+    startTime = time.time()
+    endTime = startTime + ROUND_TIME
+    inRound = True
+    timeLeft = endTime - time.time() 
+
+def end_round():
+    global inRound, timeLeft, player
+    inRound = False
+    timeLeft = 0
+    player.rect.x = WIDTH/2
+    player.rect.y = HEIGHT/2
+    player.draw(WIN)
+
 def run():
+    global inRound,timeLeft,player
     run = True
     clock = pygame.time.Clock()
     #Create the player
     player = Player((WIDTH/2,HEIGHT/2), money=35)
+    
+    
+    #start the first round
+    start_round()
+    
 
     while run:
         clock.tick(FPS)
@@ -62,17 +97,21 @@ def run():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-            
+                   
+        if inRound:
+            #handle key presses
+            keys_pressed = pygame.key.get_pressed()
+            handle_movement(keys_pressed,player)
 
-        #handle key presses
-        keys_pressed = pygame.key.get_pressed()
-        handle_movement(keys_pressed,player)
-
-        #generate coins
-        try_generate_coins()
+            #generate coins
+            try_generate_coins()
 
         #update display
-        draw_window(player,coins)
+        draw_window(player,coins,timeLeft)
+
+        timeLeft = endTime - time.time()
+        if timeLeft <= 0:
+            end_round()
 
 
 if __name__ == "__main__":
