30917845a937345a18f48612f52b8e52421dd371
johnthomsonn <johnthomson16@hotmail.co.uk>
2021-02-16 23:52:44 +0000

    added coin value variable and coin lifetime

--- Coin.py
+++ Coin.py
@@ -1,4 +1,4 @@
-import pygame, random
+import pygame, random, time
 
 YELLOW = (252,186,3)
 CYAN = (3,252,236)
@@ -7,6 +7,14 @@ PURPLE = (182,3,252)
 SMALL_SIZE = 12,12
 MEDIUM_SIZE = 18,18
 LARGE_SIZE = 24,24
+
+SMALL_TIMER = 3
+MEDIUM_TIMER = 6
+LARGE_TIMER = 10
+
+SMALL_VALUE = 10
+MEDIUM_VALUE = 5
+LARGE_VALUE = 3
 
 class Coin:
 
@@ -17,7 +25,17 @@ class Coin:
         self.size = 10
         self.position = random.randint(0,window.get_size()[0] - self.size), random.randint(0,window.get_size()[1]-self.size)
         self.rect = pygame.Rect((self.position), self.get_size())
+        self.destroy = time.time() + self.set_timer()
 
+    def set_timer(self):
+        if self.type == "large":
+            return LARGE_TIMER
+        elif self.type == "medium":
+            return MEDIUM_TIMER
+        elif self.type == "small":
+            return SMALL_TIMER
+        else:
+            return None
 
     def get_size(self):
         if self.type == "large":
@@ -31,11 +49,11 @@ class Coin:
     
     def get_value(self):
         if self.type == "small":
-            return 10
+            return SMALL_VALUE
         elif self.type == "medium":
-            return 5
+            return MEDIUM_VALUE
         elif self.type == "large":
-            return 3
+            return LARGE_VALUE
         else:
             return None
     
@@ -55,4 +73,5 @@ class Coin:
     def get_type(self):
         types = ["small", "medium", "large"]
         return types[random.randrange(0,len(types))]
+
             
